bc29e0fb59b24778223345b5f70117dbb5489249
borntosudo <192580456+borntosudo@users.noreply.github.com>
2026-09-01 19:58:16 -0700

    Update site URL in astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -12,7 +12,7 @@ import { headingNamespace } from "./src/lib/heading-namespace"
 import { headingAnchors } from "./src/lib/heading-anchors"
 
 export default defineConfig({
-  site: "https://astro-erudite.vercel.app",
+  site: "https://borntosudo.github.io",
   compressHTML: true,
   prefetch: { prefetchAll: true },
   integrations: [
